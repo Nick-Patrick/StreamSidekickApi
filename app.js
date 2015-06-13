c729ea5cfb35716f0ea35931009d1ef8347c4d82
nickpatrick <n.patrick@outlook.com>
2015-06-13 13:49:01 +0100

    Add cors - crossdomain

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 var Db = mongoose.db;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 mongoose.connect('mongodb://nick-admin:streamingmovies@ds041032.mongolab.com:41032/streamsidekick');
 var db = mongoose.connection;
 
@@ -16,7 +24,7 @@ var port = process.env.PORT || 3000;
 var amazonRecentlyAddedMoviesRouter = require('./routes/amazonRecentlyAddedMoviesRouter')(AmazonRecentlyAddedMoviesModel);
 var amazonMoviesRouter = require('./routes/amazonMoviesRouter')(AmazonMoviesModel);
 
-
+app.use(allowCrossDomain);
 app.use('/api', amazonRecentlyAddedMoviesRouter);
 app.use('/api', amazonMoviesRouter);
 
